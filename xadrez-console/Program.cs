11d0758b03a990789896c6cf05fae666445756c0
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-18 09:46:36 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,6 +16,8 @@ namespace xadrez_console
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
                 Tela.ImprimirTabuleiro(tab);
 
             }catch(TabuleiroException e)
